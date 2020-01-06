3aa47df1d86f0c5d9a4e48f97cfaf38e25f80b5c
Aurora <877286291@qq.com>
2020-01-06 22:10:22 +0800

    添加注销功能

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-button round @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    methods: {
+      logout() {
+        window.sessionStorage.clear();
+        this.$message({
+          message:'退出登录',
+          type:'warning'
+        });
+        this.$router.push('/login')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const Login = () => import('../components/Login')
+const Login = () => import('../components/Login');
+const Home = () => import('../components/Home');
 
 const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) return next('/login');
+  next()
+});
 
 export default router
