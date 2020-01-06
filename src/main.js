02e17dda7d6c46a8546a22f6ddba82a7382685f3
877286291 <877286291@qq.com>
2020-01-06 17:25:51 +0800

    login功能

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import './assets/css/global.css'
+
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:5000/'
+
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
